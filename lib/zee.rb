552431f205cb22d1d1457d33770948439616dc42
Nando Vieira <me@fnando.com>
2025-02-25 09:59:31 -0800

    Use zeitwerk instead of manually loading files.

--- lib/zee.rb
+++ lib/zee.rb
@@ -11,39 +11,16 @@ require "yaml"
 require "json"
 require "securerandom"
 
-module Zee
-  require_relative "zee/constants"
-  require_relative "zee/version"
-  require_relative "zee/app"
-  require_relative "zee/enum"
-  require_relative "zee/routes"
-  require_relative "zee/route/parser"
-  require_relative "zee/route"
-  require_relative "zee/request"
-  require_relative "zee/response"
-  require_relative "zee/renderer"
-  require_relative "zee/controller/callbacks"
-  require_relative "zee/controller/authenticity_token"
-  require_relative "zee/controller"
-  require_relative "zee/request_handler"
-  require_relative "zee/headers"
-  require_relative "zee/environment"
-  require_relative "zee/config"
-  require_relative "zee/encrypted_file"
-  require_relative "zee/main_keyring"
-  require_relative "zee/secrets"
-  require_relative "zee/middleware_stack"
-  require_relative "zee/middleware/static"
-  require_relative "zee/middleware/content_security_policy"
-  require_relative "zee/keyring"
-  require_relative "zee/encoders/json_encoder"
-  require_relative "zee/keyring/encryptor/aes"
-  require_relative "zee/keyring/key"
-  require_relative "zee/params"
-  require_relative "zee/assets_manifest"
-  require_relative "zee/view_helpers/assets"
-  require_relative "zee/view_helpers/output_safety"
-  require_relative "zee/view_helpers/html"
-  require_relative "zee/view_helpers/capture"
-  require_relative "zee/safe_buffer"
-end
+require_relative "zee/constants"
+require_relative "zee/enum"
+
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+loader.inflector.inflect(
+  "aes" => "AES",
+  "cli" => "CLI",
+  "html" => "HTML",
+  "json_encoder" => "JSONEncoder"
+)
+loader.ignore("#{__dir__}/sequel")
+loader.setup
