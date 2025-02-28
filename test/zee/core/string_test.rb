47db1269febecffdd55366de6433316d69cc0e66
Nando Vieira <me@fnando.com>
2025-02-28 11:48:22 -0800

    Add form helpers. (#42)

--- test/zee/core/string_test.rb
+++ test/zee/core/string_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 class StringTest < Minitest::Test
   using Zee::Core::String
 
-  test "converts to underscore" do
+  test "#underscore" do
     assert_equal "user", "User".underscore
     assert_equal "user_tag", "UserTag".underscore
     assert_equal "api", "API".underscore
@@ -18,10 +18,39 @@ class StringTest < Minitest::Test
     assert_equal "module/html_encoder", "Module::HTMLEncoder".underscore
   end
 
-  test "converts to camelcase" do
+  test "#camelcase" do
     assert_equal "User", "user".camelize
     assert_equal "UserTag", "user_tag".camelize
     assert_equal "Module::User", "module/user".camelize
     assert_equal "Module::UserTag", "module/user_tag".camelize
+  end
+
+  test "#humanize" do
+    assert_equal "Name", "name".humanize
+    assert_equal "Full name", "full_name".humanize
+    assert_equal "User", "user_id".humanize
+    assert_equal "User id", "user_id".humanize(keep_id_suffix: true)
+  end
+
+  test "#blank?" do
+    assert "".blank?
+    assert "   ".blank?
+    assert "\r\n\t   ".blank?
+    assert "\u00a0".blank?
+
+    refute "hello".blank?
+    refute "  hello ".blank?
+    refute "\r\n\t hello  ".blank?
+  end
+
+  test "#present?" do
+    refute "".present?
+    refute "   ".present?
+    refute "\r\n\t   ".present?
+    refute "\u00a0".present?
+
+    assert "hello".present?
+    assert "  hello ".present?
+    assert "\r\n\t hello  ".present?
   end
 end
