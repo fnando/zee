64df4629353338eb8c5b822e7637ee54804320f1
Nando Vieira <me@fnando.com>
2025-03-09 13:07:58 -0700

    Update parameter filter documentation.

--- lib/zee/parameter_filter.rb
+++ lib/zee/parameter_filter.rb
@@ -1,8 +1,33 @@
 # frozen_string_literal: true
 
 module Zee
+  # Filter parameters from logs and exceptions.
+  # This is useful to avoid leaking sensitive information.
+  #
+  # By default, the following parameter keys  are filtered:
+  #
+  # - `passw`
+  # - `email`
+  # - `secret`
+  # - `token`
+  # - `_key`
+  # - `crypt`
+  # - `salt`
+  # - `certificate`
+  # - `otp`
+  # - `ssn`
+  # - `cvv`
+  # - `cvc`
+  #
+  # > [!NOTICE]
+  # > Remember that this filter performs partial matching. For example, the
+  # > `email` filter will match any parameter key that contains the word
+  # > `email`, like `email` and `user_email`.
   class ParameterFilter
+    # @api private
     DEFAULT_MASK = "[filtered]"
+
+    # @api private
     DEFAULT_FILTERS = %w[
       passw email secret token _key crypt salt certificate otp ssn cvv cvc
     ].freeze
