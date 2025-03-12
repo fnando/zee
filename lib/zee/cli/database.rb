ea630d93a9dda918b3a80e064e83bcd419d7606b
Nando Vieira <me@fnando.com>
2025-03-12 11:17:57 -0700

    Add support for connecting to databases from the CLI.

--- lib/zee/cli/database.rb
+++ lib/zee/cli/database.rb
@@ -106,6 +106,61 @@ module Zee
                        desc: "Show more information",
                        aliases: "-v"
 
+          desc "db", "Open the database console"
+          long_desc <<~TEXT
+            Open the database console using the connection string provided in
+            the DATABASE_URL environment variable, dotenv files, or specified
+            via the `--connection-string` switch.
+
+            For SQLite databases, it will also load extensions from your local
+            `.sqlpkg` directory automatically. Currently, it is not possible to
+            load extensions from other locations. For more information on
+            installing SQLite extensions, see <https://sqlpkg.org>.
+          TEXT
+          define_common_options.call
+          # :nocov:
+          define_method :db do
+            uri = URI(db_helpers.connection_string)
+
+            args =
+              case uri.scheme
+              when "sqlite"
+                extensions =
+                  Dir[".sqlpkg/**/*.{dylib,so}"].flat_map do |file|
+                    dir = File.dirname(file)
+                    basename = File.basename(file, ".*")
+
+                    ["-cmd", ".load #{File.join(dir, basename)}"]
+                  end
+
+                unless uri.opaque == ":memory:"
+                  path = File.join(uri.hostname, uri.path)
+                end
+
+                ["sqlite3", *extensions, path].compact
+              when "postgres"
+                ["psql", db_helpers.connection_string]
+              when "mysql", "mysql2"
+                mysql_args = []
+                mysql_args.push("--user", uri.user) if uri.user
+                mysql_args.push("--host", uri.hostname) if uri.hostname
+                mysql_args.push("--port", uri.port) if uri.port
+                unless uri.path.empty?
+                  mysql_args.push("--database=name", uri.path[1..-1])
+                end
+                mysql_args.push("-p#{uri.password}") if uri.password
+
+                ["mysql", *mysql_args]
+              else
+                raise Thor::Error,
+                      set_color("Unsupported database type: #{uri.scheme}://",
+                                :red)
+              end
+
+            system(*args)
+          end
+          # :nocov:
+
           desc "db:migrate", "Migrate the database"
           define_common_options.call
           define_method :"db:migrate" do
