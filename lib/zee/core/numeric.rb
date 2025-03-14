369a3c135ce0e9c34129b683d832530ff0363bfd
Nando Vieira <me@fnando.com>
2025-03-14 09:16:28 -0700

    Add numeric extension for duration.

--- lib/zee/core/numeric.rb
+++ lib/zee/core/numeric.rb
@@ -44,6 +44,68 @@ module Zee
 
           "#{number}#{unit}"
         end
+
+        # @!method second
+        # Returns the number of seconds.
+        # @return [Numeric] the number of seconds.
+        def second
+          self
+        end
+        alias_method :seconds, :second
+
+        # @!method minute
+        # Returns the number of minutes in seconds.
+        # @return [Numeric] the number of seconds.
+        def minute
+          self * 60
+        end
+        alias_method :minutes, :minute
+
+        # @!method hour
+        # Returns the number of hours in seconds.
+        # @return [Numeric] the number of seconds.
+        def hour
+          self * 3600
+        end
+        alias_method :hours, :hour
+
+        # @!method day
+        # Returns the number of days in seconds.
+        # @return [Numeric] the number of seconds.
+        def day
+          self * 86_400
+        end
+        alias_method :days, :day
+
+        # @!method week
+        # Returns the number of weeks in seconds.
+        # @return [Numeric] the number of seconds.
+        def week
+          self * 86_400 * 7
+        end
+        alias_method :weeks, :week
+
+        # @!method month
+        # Returns the number of months in seconds.
+        # A month is considered to be 1/12 of a gregorian year, to match Rails'
+        # behavior.
+        #
+        # @return [Numeric] the number of seconds.
+        def month
+          self * 2_629_746
+        end
+        alias_method :months, :month
+
+        # @!method year
+        # Returns the number of years in seconds.
+        # A year is considered to be a full gregorian year (365.2425 days), to
+        # match Rails' behavior.
+        #
+        # @return [Numeric] the number of seconds.
+        def year
+          self * 31_556_952
+        end
+        alias_method :years, :year
       end
     end
   end
--- test/zee/core/numeric_test.rb
+++ test/zee/core/numeric_test.rb
@@ -18,4 +18,29 @@ class NumericTest < Minitest::Test
     assert_equal "100ns", 0.0000001.duration
     assert_equal "150ns", 0.00000015.duration
   end
+
+  test "returns number of seconds" do
+    assert_equal 1, 1.second
+    assert_equal 2, 2.seconds
+
+    assert_equal 60, 1.minute
+    assert_equal 120, 2.minutes
+
+    assert_equal 3600, 1.hour
+    assert_equal 7200, 2.hours
+
+    assert_equal 86_400, 1.day
+    assert_equal 172_800, 2.days
+
+    assert_equal 86_400 * 7, 1.week
+    assert_equal 86_400 * 14, 2.weeks
+
+    assert_equal 2_629_746, 1.month
+    assert_equal 2_629_746 * 2, 2.months
+
+    assert_equal 31_556_952, 1.year
+    assert_equal 31_556_952 * 2, 2.years
+
+    assert_in_delta(174.0, 2.9.minutes)
+  end
 end
