487f4f08515d50aa6b2137a5add360b359401f1b
Nando Vieira <me@fnando.com>
2025-03-22 12:17:44 -0700

    Use log connection yield api with Sequel.

--- lib/sequel/extensions/instrumentation.rb
+++ lib/sequel/extensions/instrumentation.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
+require "sequel/extensions/sql_log_normalizer"
+
 module Sequel
   module Instrumentation
-    def execute(sql, opts = OPTS, &block)
-      Zee::Instrumentation.instrument(:sequel, sql:) { super }
+    def self.normalize_sql
+      @normalize_sql ||= Sequel::SQLLogNormalizer
+                         .instance_method(:normalize_logged_sql)
+                         .bind(nil)
+    end
+
+    def log_connection_yield(sql, *)
+      return super unless Zee.app.config.enable_instrumentation
+
+      Zee::Instrumentation.instrument(
+        :sequel,
+        sql: Instrumentation.normalize_sql.call(sql)
+      ) { super }
     end
   end
 
