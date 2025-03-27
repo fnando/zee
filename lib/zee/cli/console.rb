29da44aeb204641b7008d8fda3f36c6ebd449ec6
Nando Vieira <me@fnando.com>
2025-03-27 03:19:39 -0400

    Extract the console command.

---
+++ lib/zee/cli/console.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Zee
+  module CLI
+    class Root < Command
+      desc "console", "Start a console (alias: c)"
+      option :env,
+             type: :string,
+             default: "development",
+             desc: "Set the environment",
+             aliases: "-e",
+             enum: %w[development test production]
+      # :nocov:
+      def console
+        require "bundler/setup"
+        require "dotenv"
+        require "irb"
+        require "irb/completion"
+
+        env =
+          (ENV_NAMES.filter_map do |name|
+            ENV[name]
+          end.first || options[:env]).to_sym
+
+        Dotenv.load(".env", ".env.#{env}")
+        Bundler.require(:default, env)
+        require "./config/environment"
+
+        prompt_prefix = "%N(#{set_color(PROMPT_ALIASES.fetch(env),
+                                        PROMPT_COLORS.fetch(env))})"
+
+        IRB.setup(nil)
+        IRB.conf[:PROMPT][:ZEE] = {
+          PROMPT_I: "#{prompt_prefix}> ",
+          PROMPT_S: "#{prompt_prefix}%l ",
+          PROMPT_C: "#{prompt_prefix}* ",
+          RETURN: "=> %s\n"
+        }
+        IRB.conf[:PROMPT_MODE] = :ZEE
+        IRB::Irb.new.run(IRB.conf)
+      end
+      # :nocov:
+    end
+  end
+end
--- lib/zee/cli/root.rb
+++ lib/zee/cli/root.rb
@@ -22,6 +22,7 @@ module Zee
 
       require_relative "new"
       require_relative "routes"
+      require_relative "console"
       include Database if CLI.available?("sequel")
       include Secrets
 
@@ -42,44 +43,6 @@ module Zee
       def version
         puts "zee #{VERSION}"
       end
-
-      desc "console", "Start a console (alias: c)"
-      option :env,
-             type: :string,
-             default: "development",
-             desc: "Set the environment",
-             aliases: "-e",
-             enum: %w[development test production]
-      # :nocov:
-      def console
-        require "bundler/setup"
-        require "dotenv"
-        require "irb"
-        require "irb/completion"
-
-        env =
-          (ENV_NAMES.filter_map do |name|
-            ENV[name]
-          end.first || options[:env]).to_sym
-
-        Dotenv.load(".env", ".env.#{env}")
-        Bundler.require(:default, env)
-        require "./config/environment"
-
-        prompt_prefix = "%N(#{set_color(PROMPT_ALIASES.fetch(env),
-                                        PROMPT_COLORS.fetch(env))})"
-
-        IRB.setup(nil)
-        IRB.conf[:PROMPT][:ZEE] = {
-          PROMPT_I: "#{prompt_prefix}> ",
-          PROMPT_S: "#{prompt_prefix}%l ",
-          PROMPT_C: "#{prompt_prefix}* ",
-          RETURN: "=> %s\n"
-        }
-        IRB.conf[:PROMPT_MODE] = :ZEE
-        IRB::Irb.new.run(IRB.conf)
-      end
-      # :nocov:
 
       desc "generate SUBCOMMAND", "Generate new code (alias: g)"
       subcommand "generate", Generate
