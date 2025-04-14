d7853be6c8f9b6de0fe710d19942bbf902639e2e
Nando Vieira <me@fnando.com>
2025-04-14 10:02:15 -0300

    Move table output to its own method.

--- lib/zee/cli/routes.rb
+++ lib/zee/cli/routes.rb
@@ -4,41 +4,56 @@ module Zee
   module CLI
     class Root < Command
       desc "routes", "List all routes"
+      option :format,
+             type: :string,
+             aliases: "-f",
+             default: "table",
+             enum: %w[table typescript]
       def routes
         require "bundler/setup"
         require "dotenv"
-        require "terminal-table"
 
         Dotenv.load(".env", ".env.development", ".env.test", ".env.production")
         Bundler.require(:default)
         require "./config/environment" if File.file?("./config/environment.rb")
 
-        normalize_to = proc do |to|
-          next to if to.is_a?(String)
-          next to.name if to.respond_to?(:name)
-          next to.to_s unless to.is_a?(Proc)
+        send("output_to_#{options[:format]}")
+      end
 
-          path, line = to.source_location
-          path = Pathname(path).relative_path_from(Dir.pwd)
-          "#{path}:#{line}"
+      no_commands do
+        def output_to_table
+          require "terminal-table"
+
+          normalize_to = proc do |to|
+            next to if to.is_a?(String)
+            next to.name if to.respond_to?(:name)
+            next to.to_s unless to.is_a?(Proc)
+
+            path, line = to.source_location
+            path = Pathname(path).relative_path_from(Dir.pwd)
+            "#{path}:#{line}"
+          end
+
+          routes = Zee.app.routes.to_a
+          headings = %w[Verb Path Prefix To]
+          rows = routes.map do |route|
+            [
+              route.via.map { _1.to_s.upcase }.join(", "),
+              route.path,
+              route.name,
+              normalize_to.call(route.to)
+            ]
+          end
+
+          table = ::Terminal::Table.new(rows:, headings:) do |t|
+            t.style = {
+              border_left: false, border_right: false,
+              padding_right: 5
+            }
+          end
+
+          puts table
         end
-
-        routes = Zee.app.routes.to_a
-        headings = %w[Verb Path Prefix To]
-        rows = routes.map do |route|
-          [
-            route.via.map { _1.to_s.upcase }.join(", "),
-            route.path,
-            route.name,
-            normalize_to.call(route.to)
-          ]
-        end
-
-        table = ::Terminal::Table.new(rows:, headings:) do |t|
-          t.style = {border_left: false, border_right: false, padding_right: 5}
-        end
-
-        puts table
       end
     end
   end
