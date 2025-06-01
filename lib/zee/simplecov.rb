4c1f076307f3742dd12f9b7eb4a7cc8121b6a99b
Nando Vieira <me@fnando.com>
2025-05-31 17:30:28 -0700

    Fix simplecov support.

--- lib/zee/simplecov.rb
+++ lib/zee/simplecov.rb
@@ -9,18 +9,20 @@ end
 require "simplecov"
 
 SimpleCov.profiles.define :zee do
-  track_files "app/**/*.rb"
-  add_filter ["config/", "test/", "spec/", "db/"]
+  load_profile "bundler_filter"
+  load_profile "test_frameworks"
+  add_filter %r{^/config/}
+  add_filter %r{^/db/}
 
-  {
-    "Actions" => "app/actions",
-    "Controllers" => "app/controllers",
-    "Helpers" => "app/helpers",
-    "Jobs" => "app/jobs",
-    "Mailers" => "app/mailers",
-    "Models" => "app/models",
-    "Views" => "app/views"
-  }.each do |label, dir|
-    add_group(label, dir) if Dir["#{dir}/**/*.rb"].any?
+  Dir["app/*"].each do |dir|
+    next unless File.directory?(dir)
+
+    title = File.basename(dir).tr("_", " ").capitalize
+
+    add_group(title, dir) if Dir["#{dir}/**/*.rb"].any?
   end
+
+  track_files "app/**/*.rb"
 end
+
+at_exit { SimpleCov.at_exit_behavior }
